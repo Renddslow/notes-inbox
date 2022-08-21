9c2e81da28cfd74e66acc9ca8f7a406ac195dc85
Matt McElwee <mubatt@wyopub.com>
2022-08-21 11:24:05 -0400

    Update auth.cjs

--- functions/auth.cjs
+++ functions/auth.cjs
@@ -5,6 +5,14 @@ const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
 
 const PASS = process.env.PASS;
 
+const generateCode = () => {
+    let possible = '0123456789'
+    let string = ''
+    for (let i = 0; i < 6; i++) {
+        string += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+};
+
 exports.handler = async (event) => {
     const { password } = JSON.parse(event.body);
     const isValid = await bcrypt.compare(password, PASS);
@@ -15,7 +23,7 @@ exports.handler = async (event) => {
         });
     }
 
-    const generatedCode = '';
+    const generatedCode = generateCode();
     await client.messages.create({
         body: ``,
         from: '+14023789202',
